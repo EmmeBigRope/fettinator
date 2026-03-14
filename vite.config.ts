60e6b60d75c47820938e8896d49395c4257356c6
MaCoLab <macolab.principale@gmail.com>
2026-03-14 15:04:53 +0100

    Modifiche

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/fettinator/',   // <-- Aggiungi questa riga con il nome del repository
+    base: '/fettinator/',   // <---- QUESTO È FONDAMENTALE
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
